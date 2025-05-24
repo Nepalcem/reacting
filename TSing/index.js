7a00cb7994aae141c86c497a1b19e6de94e3338b
MarkW <mark.emailtest@gmail.com>
2025-05-24 23:42:11 +0300

    ts init

---
+++ TSing/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function summ(a, b) {
+    return a + b;
+}
+console.log(summ(1, 2));
---
+++ TSing/index.ts
@@ -0,0 +1,5 @@
+function summ(a:number, b:number) :number {
+  return a + b;
+}
+
+console.log(summ(1, 2));
