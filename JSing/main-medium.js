2f8099f1f24dd4d6cfbc87ec4c9c9a2f04c92220
MarkW <mark.emailtest@gmail.com>
2025-06-25 20:50:22 +0300

    object copying

--- JSing/main-medium.js
+++ JSing/main-medium.js
@@ -1,19 +1,29 @@
-const repeat = function (t) {
-  console.log(`This text appears after ${t} seconds`);
-};
+// const repeat = function (t) {
+//   console.log(`This text appears after ${t} seconds`);
+// };
 
-function debounce(fn, timer) {
-  let timeOut;
-  return function () {
-    clearTimeout(timeOut);
-    timeOut = setTimeout(() => {
-      fn(timer);
-    }, timer);
-  };
+// function debounce(fn, timer) {
+//   let timeOut;
+//   return function () {
+//     clearTimeout(timeOut);
+//     timeOut = setTimeout(() => {
+//       fn(timer);
+//     }, timer);
+//   };
+// }
+
+// const debounceFunction = debounce(repeat, 2000);
+
+// debounceFunction();
+// debounceFunction();
+// debounceFunction();
+
+const objectToCopy = { a: 1, b: "text", c: { d: 2, e: "new" } };
+
+function deepCopy(obj) {
+    return JSON.parse(JSON.stringify(obj));
 }
 
-const debounceFunction = debounce(repeat, 2000);
 
-debounceFunction();
-debounceFunction();
-debounceFunction();
+
+console.log(deepCopy(objectToCopy));
