afeb583f0e64c0e284e74f85e58a3a3de296a92f
MarkW <mark.emailtest@gmail.com>
2025-06-30 21:47:39 +0300

    char freq & arrays

--- JSing/jsing-block2.js
+++ JSing/jsing-block2.js
@@ -9,25 +9,57 @@ function capitalizeWords(str) {
     .join(" ");
 }
 
-console.log(capitalizeWords("hello world"));
+// console.log(capitalizeWords("hello world"));
 
 // Input: "racecar" → true; "hello" → false
 function isPalindrome(str) {
-
   if (str.length % 2 === 1) {
-      return str
-          .slice(0, Math.floor(str.length / 2))
-          .split("")
-          .reverse()
-          .join("") === str.slice(Math.ceil(str.length / 2));
-  } else if (str.length % 2 === 0){
-      return str
-          .slice(0, str.length / 2)
-          .split("")
-          .reverse()
-          .join("") === str.slice(str.length / 2);
+    return (
+      str
+        .slice(0, Math.floor(str.length / 2))
+        .split("")
+        .reverse()
+        .join("") === str.slice(Math.ceil(str.length / 2))
+    );
+  } else if (str.length % 2 === 0) {
+    return (
+      str
+        .slice(0, str.length / 2)
+        .split("")
+        .reverse()
+        .join("") === str.slice(str.length / 2)
+    );
   }
 }
 
-console.log(isPalindrome("racecar"));
-console.log(isPalindrome("braarb"));
+// 3. Count Character Frequency
+// Input: "banana" → Output: { b: 1, a: 3, n: 2 }
+function charFrequency(str) {
+  const strArr = str.split("");
+  const result = {};
+  for (const element of strArr) {
+    if (!result[element]) {
+      result[element] = 1;
+    } else {
+      result[element]++;
+    }
+  }
+  return result;
+}
+
+// console.log(charFrequency("banana"));
+
+
+// 4. Remove Duplicates from Array
+// Input: [1, 2, 2, 3] → Output: [1, 2, 3]
+function removeDuplicates(arr) {
+  const newArr = [];
+  arr.forEach(el => {
+    if (!newArr.includes(el)) {
+      newArr.push(el);
+    }
+  })
+  return newArr;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3]));
