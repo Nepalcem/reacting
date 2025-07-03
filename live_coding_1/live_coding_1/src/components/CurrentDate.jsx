5d28db5e0603518825ddaa406fee436139a9084f
MarkW <mark.emailtest@gmail.com>
2025-07-03 22:27:24 +0300

    timer control

--- live_coding_1/live_coding_1/src/components/CurrentDate.jsx
+++ live_coding_1/live_coding_1/src/components/CurrentDate.jsx
@@ -2,14 +2,45 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 export default function CurrentDate() {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(0);
+  const [isTimerEnabled, setIsTimerEnabled] = useState(false);
+
+  function padConvert(time) {
+    const secondsLeft = time % 60;
+    const minutes =
+      time >= 3600 ? Math.floor((time % 3600) / 60) : Math.floor(time / 60);
+    const hours = Math.floor(time / 3600);
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${secondsLeft.toString().padStart(2, "0")}`;
+  }
+
+  let currentTime = padConvert(currentDate);
+
+  function startTimer() {
+    setIsTimerEnabled(!isTimerEnabled);
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentDate(new Date())
-    }, 1000);
+    let interval;
+    if (isTimerEnabled) {
+      interval = setInterval(() => {
+        setCurrentDate(currentDate + 1);
+      }, 1000);
+    }
 
     return () => clearInterval(interval);
   });
-  return <div>{currentDate.toLocaleString()}</div>;
+
+  return (
+    <>
+      <div>{currentTime ?? `00:00:00`}</div>
+      <button type="button" onClick={() => startTimer()}>
+        Start
+      </button>
+      <button type="button" onClick={() => startTimer()}>
+        Stop
+      </button>
+    </>
+  );
 }
